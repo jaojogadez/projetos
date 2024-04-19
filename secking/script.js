ec17a34e304f46201d1c69eab21cb4b62c6d1d49
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-04-19 12:24:18 -0300

    update

--- secking/script.js
+++ secking/script.js
@@ -1,5 +1,5 @@
 
-window.addEventListener("scroll", function(){
+//window.addEventListener("scroll", function(){
     let nav = document.querySelector('#navbar')
     nav.classList.toggle('rolagem',window.scrollY > 0)
 })
