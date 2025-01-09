d4fd5786d292d6fa7ce0f4588626a222f2ab7c7f
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-09 10:49:38 -0300

    terminei

--- Testes/upload file/script.js
+++ Testes/upload file/script.js
@@ -13,11 +13,27 @@ label.addEventListener("drop", onLeave)
 label.addEventListener("dragend", onLeave)
 label.addEventListener("dragleave", onLeave)
 
-const input = document.querySelector("#file")
+const input = document.querySelector("input")
 const dropzone = document.querySelector("#drop-zone")
 
-input.addEventListener("change", event => {
-    if(input.files.lenght > 0){
+input.addEventListener("change", () => {
+    if(input.files.length > 0){
         console.log(input.files)
+        const type = input.files[0].type
+        const formats = ["image/jpeg", "image/png", "image/jpg"]
+        if(!formats.includes(type)){
+            alert("Formato não permitido")
+            return
+        }
+        
+        if(document.querySelector("#cover")){
+            dropzone.removeChild(document.querySelector("#cover"))
+        }
+
+        const img = document.createElement("img")
+        img.id = "cover"
+        img.src =   URL.createObjectURL(input.files[0])
+        
+        dropzone.appendChild(img)
     }
 })
